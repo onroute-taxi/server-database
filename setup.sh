4135ffc0ddf22634004f35493336567d33c9eb32
Steven Enamakel <steven.enamakel@gmail.com>
2016-03-31 14:21:46 +0300

    minor changes

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Download Neo4j
+wget http://neo4j.com/artifact.php?name=neo4j-community-2.3.2-unix.tar.gz
+tar xvf neo4j-community-2.3.2-unix.tar.gz
+mv ./neo4j-community-2.3.2-unix ./neo4j
