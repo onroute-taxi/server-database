06b6a17974d059f77721307bed8857c8208d1c01
Steven Enamakel <steven.enamakel@gmail.com>
2016-04-28 15:03:28 +0530

    Added kernel extension

---
+++ src/main/java/com/backseatmedia/database/OnrouteKernelExtension.java
@@ -0,0 +1,27 @@
+package com.backseatmedia.database;
+
+import org.neo4j.kernel.lifecycle.Lifecycle;
+
+/**
+ * @author steven.enamakel@gmail.com
+ */
+public class OnrouteKernelExtension implements Lifecycle{
+    @Override public void init() throws Throwable {
+        System.out.println("fucking init");
+    }
+
+
+    @Override public void start() throws Throwable {
+
+    }
+
+
+    @Override public void stop() throws Throwable {
+
+    }
+
+
+    @Override public void shutdown() throws Throwable {
+
+    }
+}
---
+++ src/main/java/com/backseatmedia/database/OnrouteKernelExtensionFactory.java
@@ -0,0 +1,29 @@
+package com.backseatmedia.database;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.kernel.configuration.Config;
+import org.neo4j.kernel.extension.KernelExtensionFactory;
+import org.neo4j.kernel.impl.spi.KernelContext;
+import org.neo4j.kernel.lifecycle.Lifecycle;
+
+/**
+ * @author steven.enamakel@gmail.com
+ */
+public class OnrouteKernelExtensionFactory extends KernelExtensionFactory<OnrouteKernelExtensionFactory.Dependencies> {
+    protected OnrouteKernelExtensionFactory(String key) {
+        super("OnrouteExtension");
+        System.out.printf("initinit");
+    }
+
+
+    public interface Dependencies {
+        GraphDatabaseService getGraphDatabaseService();
+        Config getConfig();
+    }
+
+
+    @Override public Lifecycle newInstance(KernelContext context, Dependencies dependencies) throws Throwable {
+        System.out.println("fuck you");
+        return new OnrouteKernelExtension();
+    }
+}
